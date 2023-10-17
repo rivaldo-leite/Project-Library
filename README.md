9cbbfd6e4a645ed8e4c5beef3613ae11b6fab2d1
Rivaldo Leite <140112307+rivaldo-leite@users.noreply.github.com>
2023-10-17 15:25:59 -0300

    Funcionalidades adicionadas e projeto finalizado

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Project-Library
+
+Projeto inspirado na tarefa do The Odin Project.
--- script.js
+++ script.js
@@ -8,25 +8,69 @@ const inputAuthor = document.querySelector('#author');
 const inputPages = document.querySelector('#pages');
 const radioButton = document.querySelector('input[name="read-status]');
 const clearButton = document.querySelector('#clear-button');
+const bookShelf = document.querySelector('.book-shelf');
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
 }
 
-function addBookToLibrary(title, author, pages, read) {
-    const newBook = new Book(title, author, pages, read);
+function addBookToLibrary(title, author, pages) {
+    const newBook = new Book(title, author, pages);
     myLibrary.push(newBook);
 }
 
-function clearRadio() {
-    let readButton = document.getElementById("read");
-    let notReadButton = document.getElementById("not-read");
-    readButton.checked = false;
-    notReadButton.checked = false;
+function getTitle(library, index) {
+    let titleBook = library[index].title;
+    return titleBook;
 }
+
+function getAuthor(library, index) {
+    let authorBook = library[index].author;
+    return authorBook;
+}
+
+function getPages(library, index) {
+    let pagesBook = library[index].pages;
+    return pagesBook;
+}
+
+function addCard(library, index) {
+    const newCard = document.createElement('div');
+    newCard.classList = "card";
+    const titleCard = document.createElement('p');
+    titleCard.innerHTML = `Title: ${getTitle(library, index)}`;
+    const authorCard = document.createElement('p');
+    authorCard.innerHTML = `Author: ${getAuthor(library, index)}`;
+    const pagesCard = document.createElement('p');
+    pagesCard.innerHTML = `Pages: ${getPages(library, index)}`;
+    const divStatus = document.createElement('div');
+    divStatus.classList = 'div-status';
+    const yesButton = document.createElement('button');
+    yesButton.innerHTML = 'Read';
+    yesButton.classList = 'btn-read';
+    const noButton = document.createElement('button');
+    noButton.innerHTML = 'Not Read';
+    noButton.classList = 'btn-not-read';
+    const removeCard = document.createElement('button');
+    removeCard.classList = "remove-button";
+    removeCard.innerHTML = 'X';
+
+    newCard.appendChild(removeCard);
+    newCard.appendChild(titleCard);
+    newCard.appendChild(authorCard);
+    newCard.appendChild(pagesCard);
+    divStatus.appendChild(yesButton);
+    divStatus.appendChild(noButton);
+    newCard.appendChild(divStatus);
+    bookShelf.appendChild(newCard);
+
+    removeCard.addEventListener("click", () => {
+        newCard.remove();
+    })
+}
+
 
 showForm.addEventListener('click', () => {
     dialog.showModal();
@@ -36,18 +80,26 @@ addBook.addEventListener('click', () => {
     let titleName = document.querySelector("#title").value;
     let authorName = document.querySelector("#author").value;
     let totalPages = document.querySelector("#pages").value;
-    let readStatus = document.querySelector('input[name="read-status"]:checked').value;
-    addBookToLibrary(titleName.value, authorName, totalPages, readStatus);
-    
+    if ((titleName.length > 0) && (authorName.length > 0) && (totalPages.length > 0)) {
+        addBookToLibrary(titleName, authorName, totalPages);
+        addCard(myLibrary, 0);
+        myLibrary.shift();
+        inputTitle.value = '';
+        inputAuthor.value = '';
+        inputPages.value = '';
+    }
 })
+
 
 clearButton.addEventListener('click', () => {
     inputTitle.value = '';
     inputAuthor.value = '';
     inputPages.value = '';
-    clearRadio();
 })
 
 closeForm.addEventListener('click', () => {
+    inputTitle.value = '';
+    inputAuthor.value = '';
+    inputPages.value = '';
     dialog.close();
 })
